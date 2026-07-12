34e8a2b3980308b05bdf3d1ebf769066470f9239
yusufarab1 <mohamedyusuf318@outlook.com>
2026-07-12 00:52:32 -0500

    remove thinkingConfig

--- api/generate.js
+++ api/generate.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         contents: [{ parts: [{ text: prompt }] }],
         generationConfig: {
           temperature: 0.9,
-          maxOutputTokens: 2048,
+          maxOutputTokens: 8192,
           responseMimeType: 'application/json',
         },
       }),
